1ad5247b5d3fd88154b68fe8f86baa72884c17b9
Aayush <aayush17125@iiitd.ac.in>
2018-04-13 17:49:42 +0530

    RFID updated

--- RFID/sketch_apr10a/sketch_apr10a.ino
+++ RFID/sketch_apr10a/sketch_apr10a.ino
@@ -2,7 +2,14 @@
 #include <MFRC522.h> 
 #define SS_PIN 10
 #define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
+
+String master;
+String prog1 = "";
+String prog2 = "";
+String prog3 = "";
+int key = 0;
+
 void setup() 
 {
   Serial.begin(9600);   // Initiate a serial communication
@@ -10,6 +17,11 @@ void setup()
   mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("Approximate your card to the reader...");
   Serial.println();
+  master = "23 E3 54 C3";
+  prog1 = "";
+  prog2 = "";
+  prog3 = "";
+  key = 0;
 
 }
 void loop() 
@@ -35,18 +47,82 @@ void loop()
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  Serial.println();
-  Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "E1 2E 16 2D" ||content.substring(1) == "23 E3 54 C3" ) //change here the UID of the card/cards that you want to give access
+  Serial.println();
+  if (content.substring(1) == master) {
+    progMode();
+    Serial.println("Progged");
+  }
+  else {
+    Serial.println(prog1);
+    if (content.substring(1) == prog1 || content.substring(1) == prog2 || content.substring(1) == prog3){
+      Serial.println("Authorized");
+    }
+    else {
+      Serial.println("UnAuth");
+    }
+  }
+  delay(3000);
+}
+
+String fix(String progg){
+  String t = "";
+  for(int i=0; i<progg.length(); i++){
+    if(i != 0){
+      t += progg[i];
+    }
+  }
+  return(t);
+}
+
+void progMode(void) {
+  Serial.println("programming now");
+  delay(2000);
+  String contentt= "";
+  byte letter;
+  mfrc522.PICC_IsNewCardPresent();
+  mfrc522.PICC_ReadCardSerial();
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
-    Serial.println("Authorized access");
-    Serial.println();
-    delay(3000);
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     contentt.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     contentt.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
- 
- else   {
-    Serial.println(" Access denied");
-    delay(3000);
+  Serial.println("contentt " + contentt);
+  contentt.toUpperCase();
+  if(prog1 == "") {
+    prog1 = contentt;
+    prog1 = fix(prog1);
+    Serial.println(prog1 + " Lol");
   }
-} 
+  else if(prog2 == "") {
+    Serial.println(prog2);
+    prog2 = contentt;
+    prog2 = fix(prog2);
+  }
+  else if(prog3 == "") {
+    Serial.println(prog3);
+    prog3 = contentt;
+    prog3 = fix(prog3);
+  }
+  else {
+    key += 1;
+    if(key % 3 == 1) {
+      Serial.println(prog1);
+      prog1 = contentt;
+      prog1 = fix(prog1);
+    }
+    else if(key %3 == 2) {
+      Serial.println(prog2);
+      prog2 = contentt;
+      prog2 = fix(prog2);
+    }
+    else {
+      Serial.println(prog3);
+      prog3 = contentt;
+      prog3 = fix(prog3);
+    }
+  }
+}
+
